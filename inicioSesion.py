fa3e4dbe86c844f2521bfa9bb517d7f2eaea7c8c
arianpalacios2009 <arian.palacios@sagradoalcal.edu.ar>
2025-10-29 17:07:20 -0300

    se cambio los colores de la ventana home y el nombre y contraseña del usuario

--- inicioSesion.py
+++ inicioSesion.py
@@ -11,17 +11,17 @@ def home():
     home.title("Inicio")
     home.geometry("320x400")
 
-    cuadro2 = tk.Frame(home, bg="violet", bd=3, relief="ridge")
+    cuadro2 = tk.Frame(home, bg="yellow", bd=3, relief="ridge")
     cuadro2.pack(pady=40, padx=20, fill="both", expand=True)
 
-    bienvenida = tk.Label(cuadro2, text="Bienvenido a Instagram", bg="violet")
+    bienvenida = tk.Label(cuadro2, text="Bienvenido a Instagram", bg="yellow")
     bienvenida.pack(pady=80)
 
 cuadro = tk.Frame(ventana, bg="pink", bd=3, relief="ridge")
 cuadro.pack(pady=40, padx=20, fill="both", expand=True)
 
 
-titulo_labapp = tk.Label(cuadro, text= "instagram", font= "Chango", bg= "pink")
+titulo_labapp = tk.Label(cuadro, text= "instagram", font= "arial", bg= "pink")
 titulo_labapp.pack(pady=15)
 
 
@@ -37,8 +37,8 @@ entrada_contrasena = tk.Entry(cuadro)
 entrada_contrasena.pack(pady=5)
 
 
-usuario_correcto = "arian"
-contrasena_correcta = "1234"
+usuario_correcto = "jokernull"
+contrasena_correcta = "1"
 
 
 def iniciar_sesion():
@@ -56,8 +56,8 @@ def salir():
 boton = tk.Button(cuadro, text="Inciar sesion",bg= "green",  command=iniciar_sesion)
 boton.pack(pady=10)
 
-boton_salir = tk.Button(cuadro, text= "Salir", bg= "red",command=salir)
-boton_salir.pack()
+boton_salida= tk.Button(cuadro, text= "Salir", bg= "red",command=salir)
+boton_salida.pack()
 
 
 saludo = tk.Label(cuadro, text="", bg= "pink")
